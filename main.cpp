5e3c829826d0b387a189a6d62955e349a45636fe
Pykana <eduardo.elias.1998@gmail.com>
2024-03-04 13:53:00 -0500

    creacion de carpetas

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <allegro.h>
+#include <conio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <winalleg.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+}
+
+END_OF_MAIN()
